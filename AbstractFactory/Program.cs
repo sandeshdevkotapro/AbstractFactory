ac2472ec39e4ba3aed0a3799dd1a65f7e1bc84f2
sandeshdevkotapro <143295888+sandeshdevkotapro@users.noreply.github.com>
2023-12-04 23:47:20 +0200

    Update Program.cs

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -1,116 +1,139 @@
-﻿using System;
-
 namespace DesignPatterns
 {
-    // Singleton pattern for Printer class
-    public sealed class Printer
+    // Class for Printer
+    // TODO#1: Convert to use Singleton pattern
+    public class Printer
     {
-        private Printer() { }
-        private static Printer? instance;
-
-        public static Printer GetInstance()
+        private static Printer? _instance;
+        private Printer()
         {
-            if (instance == null)
+            // Private constructor.
+        }
+        public static Printer Instance()
+        {
+            if (_instance == null)
             {
-                instance = new Printer();
+                _instance = new Printer();
             }
-            return instance;
+            return _instance;
         }
 
         public void Print(string message)
         {
             Console.WriteLine(message);
-            Console.WriteLine();
+            // Output: print out the string message 
         }
     }
 
-    // Interface for different exam types
+    // Class template for Exam classes
+    // TODO#2: Convert to use Abstract Factory pattern
+    // Create an Exam interface and an Abstract Factory to manage the creation of different exam types.
+    //Exam interface 
     public interface IExam
     {
         void Conduct();
         void Evaluate();
         void PublishResults();
     }
+    // Create a abstract factory for exams
+    public abstract class ExamFactory
+    {
+        public abstract IExam CreateExam();
+    }
 
-    // MathExam class
+    //Concrete factory implementation of exams
     public class MathExam : IExam
     {
         public void Conduct()
         {
-            Printer.GetInstance().Print("Conducting Math Exam");
+            
+            Printer.Instance().Print("Conducting Math Exam");
+            // Output: "Conducting Math Exam", should use Printer class to print the message
         }
 
         public void Evaluate()
         {
-            Printer.GetInstance().Print("Evaluating Math Exam");
+            // Output: "Evaluating Math Exam", should use Printer class to print the message
+            Printer.Instance().Print("Evaluating Math Exam");
         }
 
         public void PublishResults()
         {
-            Printer.GetInstance().Print("Publishing Math Exam Results");
+            // Output: "Publishing Math Exam Results", should use Printer class to print the message
+            Printer.Instance().Print("Publishing Math Exam Results \n");
+        }
+    }
+    //Concrete factory for maths exams:
+    public class MathExamFactory : ExamFactory
+    {
+        public override IExam CreateExam()
+        {
+            return new MathExam();
         }
     }
 
-    // ScienceExam class
+
+    // TODO#5: Add new ScienceExam class
+    //Concrete factory implementation of exams
     public class ScienceExam : IExam
     {
         public void Conduct()
         {
-            Printer.GetInstance().Print("Conducting Science Exam");
+
+            Printer.Instance().Print("Conducting Science Exam");
+            // Output: "Conducting Math Exam", should use Printer class to print the message
         }
 
         public void Evaluate()
         {
-            Printer.GetInstance().Print("Evaluating Science Exam");
+            // Output: "Evaluating Math Exam", should use Printer class to print the message
+            Printer.Instance().Print("Evaluating Science Exam");
         }
 
         public void PublishResults()
         {
-            Printer.GetInstance().Print("Publishing Science Exam Results");
+            // Output: "Publishing Math Exam Results", should use Printer class to print the message
+            Printer.Instance().Print("Publishing Science Exam Results \n");
+        }
+    }
+    //Concrete factory for science exams:
+    public class ScienceExamFactory : ExamFactory
+    {
+        public override IExam CreateExam()
+        {
+            return new ScienceExam();
         }
     }
 
-    // ProgrammingExam class
+    // TODO#6: Add another ProgrammingExam class
+    //Concrete factory implementation of exams
     public class ProgrammingExam : IExam
     {
         public void Conduct()
         {
-            Printer.GetInstance().Print("Conducting Programming Exam");
+
+            Printer.Instance().Print("Conducting Programming Exam");
+            // Output: "Conducting Math Exam", should use Printer class to print the message
         }
 
         public void Evaluate()
         {
-            Printer.GetInstance().Print("Evaluating Programming Exam");
+            // Output: "Evaluating Math Exam", should use Printer class to print the message
+            Printer.Instance().Print("Evaluating Programming Exam");
         }
 
         public void PublishResults()
         {
-            Printer.GetInstance().Print("Publishing Programming Exam Results");
+            // Output: "Publishing programming Exam Results", should use Printer class to print the message
+            Printer.Instance().Print("Publishing Programming Exam Results \n");
         }
     }
-
-    // Abstract Factory for creating different exam types
-    public interface IExamFactory
+    //Concrete factory for programming exams:
+    public class ProgrammingExamFactory : ExamFactory
     {
-        IExam CreateExam(string examType);
-    }
-
-    // Concrete ExamFactory class
-    public class ExamFactory : IExamFactory
-    {
-        public IExam CreateExam(string examType)
+        public override IExam CreateExam()
         {
-            switch (examType)
-            {
-                case "Math":
-                    return new MathExam();
-                case "Science":
-                    return new ScienceExam();
-                case "Programming":
-                    return new ProgrammingExam();
-                default:
-                    throw new ArgumentException("Invalid exam type");
-            }
+            return new ProgrammingExam();
         }
     }
 
@@ -119,26 +142,40 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-            // Initialize Printer using Singleton pattern
-            Printer printer = Printer.GetInstance();
+            // TODO#7: Initialize Printer that uses Singleton pattern
+            Printer p1 = Printer.Instance();
+           
+           
+            // TODO#8: Test that the created Printer works, by calling the Print method
+            p1.Print("This is a test message");
 
-            // Test that the created Printer works by calling the Print method
-            printer.Print("This is a test message.");
 
-            // Ensure that only one Printer instance is used throughout the application
-            Printer anotherPrinter = Printer.GetInstance();
-            Console.WriteLine($"Are the two printer instances the same? {printer == anotherPrinter}");
+            // TODO#9: Ensure that only one Printer instance is used throughout the application.
+            //         Try to create new Printer object and compare the two objects to check,
+            //         that the new printer object is the same instance
+            Printer p2 = Printer.Instance();
+            bool isSameInstance = ReferenceEquals(p1, p2);
+            Console.WriteLine("It is same instance: " + isSameInstance); //Gives true if it's same instance false if it's not.
 
-            // Use Abstract Factory to create different types of exams
-            IExamFactory examFactory = new ExamFactory();
-            IExam mathExam = examFactory.CreateExam("Math");
-            IExam scienceExam = examFactory.CreateExam("Science");
-            IExam programmingExam = examFactory.CreateExam("Programming");
-
-            // Call methods on the created exams
+            // TODO#10: Use Abstract Factory to create different types of exams.
+            ExamFactory mathExamFactory = new MathExamFactory();
+            IExam mathExam = mathExamFactory.CreateExam();
             mathExam.Conduct();
+            mathExam.Evaluate();
+            mathExam.PublishResults();
+
+            ExamFactory scienceExamFactory = new ScienceExamFactory();
+            IExam scienceExam = scienceExamFactory.CreateExam();
             scienceExam.Conduct();
-            programmingExam.Conduct();
+            scienceExam.Evaluate();
+            scienceExam.PublishResults();
+
+            ExamFactory programmingExamFactory = new ProgrammingExamFactory();
+            IExam progExam = programmingExamFactory.CreateExam();
+            progExam.Conduct();
+            progExam.Evaluate();
+            progExam.PublishResults();
         }
     }
+
 }
